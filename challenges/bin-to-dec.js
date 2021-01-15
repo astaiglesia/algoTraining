f7db380c03943bb515ef8084f0f2723829b38f48
Alfred Sta Iglesia <a.sta.iglesia@gmail.com>
2021-01-15 09:55:06 -0800

    daily challenge complete -> bin-to-dec

--- challenges/bin-to-dec.js
+++ challenges/bin-to-dec.js
@@ -15,10 +15,45 @@ https://m.wikihow.com/Read-Binary
 
 */
 
-function binToDec(binString){
+// reading binary -> read from right to left
+  // each string character is raised to the power of n where n increases sequentially from right to left
+  // sum the total 
 
+// create a func; accepts a string (binary number representation); returns a num  ### DO NOT USE parseInt()
+    // iterate from right to left -> split string to create an iterable array, reduce right
+    // conditionally increment output by base 2 raised to power -> Math.pow; if 1, increment, if 0 don't
+    // increment power on each pass
+    // return total
+
+// const binToDec = (binString, power = 0) => {
+// //   console.log(binString.split(""));
+
+//   return binString.split("").reduceRight((total, current) => {
+//     // console.log(Math.pow(Number(current), power));
+//     if (current === '1') {
+//         total = total + Math.pow(2, power);
+//     }
+//     power = power + 1;
+//     return total;
+//   }, 0)
+// }
+
+// refactor code
+
+const binToDec = (binString, power = 0) => {
+  return binString.split("").reduceRight((total, current) => {
+    if (current === '1')  total = total + Math.pow(2, power);
+    power += 1;
+    return total;
+    }, 0)
 }
 
 
+// test cases
+console.log(binToDec('0'))    //-> 0
+console.log(binToDec('11'))   //-> 3
+console.log(binToDec('100'))  //-> 4
+console.log(binToDec('101'))  //-> 5
+console.log(binToDec('0101')) // -> 5
 
-module.exports = {binToDec};
+// module.exports = {binToDec};
