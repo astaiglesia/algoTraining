a79d1b4f86d03338d5f7920d5822b0f3ad60e575
Alfred Sta Iglesia <a.sta.iglesia@gmail.com>
2021-01-20 09:31:59 -0800

    hack hour submission 1.20.21

--- challenges/pow.js
+++ challenges/pow.js
@@ -7,10 +7,22 @@
  * Extension: Use recursion
  */
 
-function pow(base, power) {
-  // for a recursive approach remember to write your base case
-  // write your recursive logic here
+// for a recursive approach remember to write your base case
+// write your recursive logic here
+// ------
+// recursively multiply the base upon itself pow times --> create output var defaulted to base
+//    decrement power on each pass
+//    terminate recursion and return output when pow =0 <--- base case
+
+function pow(base, power, output = base) {
+  return (power === 1 ) ? output
+      : pow(base, power - 1, output * base);
 }
+
+ // test cases
+ console.log(pow(2,4)) // => 2^4 = 16
+ console.log(pow(3,3)) // => 3^3 = 27
+
 
 /**
  * Extension: Use recursion to solve the problem in O(n) time complexity -> Linear time complexity
@@ -18,7 +30,11 @@ function pow(base, power) {
 
 function powRecurse(base, power) {
   // for a recursive approach remember to write your base case
-  // write your recursive logic here
+  // The number of computations for an algorithm in O(n), or linear, time complexity increases in direct proportion to the size of the input data. 
+  // In this case, the power is the dictating variable as its size directly determines the number of iterations with recursive calls occuring only once per pass.
+  // Therefore, the function as written above and below are in O(n) time complexity.
+  return (power === 1 ) ? output
+  : pow(base, power - 1, output * base)
 }
 
 module.exports = { pow, powRecurse };
