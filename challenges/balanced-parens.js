d60244c545d233d68975e6370909f22329fe2b96
Alfred Sta Iglesia <a.sta.iglesia@gmail.com>
2021-01-26 09:54:36 -0800

    hack hour submission 1.26.21

--- challenges/balanced-parens.js
+++ challenges/balanced-parens.js
@@ -24,8 +24,81 @@
  *
  */
 
-const balancedParens = input => {
+// ------ Step 1
+// function accepts a string, returns a bool
+// --- conditional statement checking if opening parens are matched to a closing parens
+// ----- create a cache to match opening and closing parens
+// ----- push opening parens into the array and pop if closing parens seen
+// --- check cache at index length-1
+// --- return true if iterated through string and cache is empty (i.e. all parens have been matched)
+// --- else return false
+// consider edge cases -> not a string, not parens
+// const balancedParens = (input, cache = []) => {
+//   // base cases
+//   if (input.length === 0 && cache.length === 0) return true 
+//   if (input.length === 0 && cache.length > 0) return false
+//   if (input[0] === ')' && cache.length === 0) return false
 
-};
+//   // recursive case
+//   if (input[0] === '(') cache.push(input[0])
+//   if (input[0] === ')' && cache[cache.length-1] === '(') cache.pop()
+
+//   return balancedParens(input.slice(1), cache);
+// };
+
+
+
+// ------ Step 2
+// refactor step 1 to accomodate all brackets
+
+// const balancedParens = (input, cache = []) => {
+//   // base cases
+//   if (input.length === 0 && cache.length === 0) return true 
+//   if (input.length === 0 && cache.length > 0) return false
+//   if (input[0] === ')' && cache.length === 0) return false              // <--- closing brackets with an empty cache
+//   if (input[0] === ']' && cache.length === 0) return false 
+//   if (input[0] === '}' && cache.length === 0) return false 
+
+
+//   // recursive case
+//   if (input[0] === '(' || input[0] === '[' || input[0] === '{') cache.push(input[0])
+//   if (input[0] === ')' && cache[cache.length-1] === '(') cache.pop()
+//   if (input[0] === ']' && cache[cache.length-1] === '[') cache.pop()
+//   if (input[0] === '}' && cache[cache.length-1] === '{') cache.pop()
+
+//   return balancedParens(input.slice(1), cache);
+// };
+
+// ----- Step 3
+// refactor step 2 to eliminate non bracket characters
+// step 2 code already eliminates non bracket chars from consideration
+const balancedParens = (input, cache = []) => {
+    // base cases
+    if (input.length === 0 && cache.length === 0) return true 
+    if (input.length === 0 && cache.length > 0) return false
+    if (input[0] === ')' && cache.length === 0) return false              // <--- closing brackets with an empty cache
+    if (input[0] === ']' && cache.length === 0) return false 
+    if (input[0] === '}' && cache.length === 0) return false 
+  
+  
+    // recursive case
+    if (input[0] === '(' || input[0] === '[' || input[0] === '{') cache.push(input[0])
+    if (input[0] === ')' && cache[cache.length-1] === '(') cache.pop()
+    if (input[0] === ']' && cache[cache.length-1] === '[') cache.pop()
+    if (input[0] === '}' && cache[cache.length-1] === '{') cache.pop()
+  
+    return balancedParens(input.slice(1), cache);                         // <--- eliminates non bracket chars from consideration
+  };
+// test cases
+console.log(balancedParens('('));  // false
+console.log(balancedParens('()')); // true
+console.log(balancedParens(')('));  // false
+console.log(balancedParens('(())'));  // true
+console.log(balancedParens('[](){}')); // true
+console.log(balancedParens('[({})]'));   // true
+console.log(balancedParens('[(]{)}')); // false
+console.log(balancedParens(' const wow = { yo: thisIsAwesome() }')); // true
+console.log(balancedParens(' const newton = () => { telescopes.areSicc(); ')); // false
+
 
 module.exports = { balancedParens} ;
