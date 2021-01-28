ff08b6756ae6c5c8bca733dc39c7d7664220ac95
Alfred Sta Iglesia <a.sta.iglesia@gmail.com>
2021-01-28 09:19:45 -0800

    refactor

--- challenges/balanced-parens.js
+++ challenges/balanced-parens.js
@@ -51,54 +51,53 @@
 // ------ Step 2
 // refactor step 1 to accomodate all brackets
 
-// const balancedParens = (input, cache = []) => {
-//   // base cases
-//   if (input.length === 0 && cache.length === 0) return true 
-//   if (input.length === 0 && cache.length > 0) return false
-//   if (input[0] === ')' && cache.length === 0) return false              // <--- closing brackets with an empty cache
-//   if (input[0] === ']' && cache.length === 0) return false 
-//   if (input[0] === '}' && cache.length === 0) return false 
+const balancedParens = (input, cache = []) => {
+  // base cases
+  if (input.length === 0 && cache.length === 0) return true 
+  if (input.length === 0 && cache.length > 0) return false
+  if (input[0] === ')' && cache.length === 0) return false              // <--- closing brackets with an empty cache
+  if (input[0] === ']' && cache.length === 0) return false 
+  if (input[0] === '}' && cache.length === 0) return false 
 
 
-//   // recursive case
-//   if (input[0] === '(' || input[0] === '[' || input[0] === '{') cache.push(input[0])
-//   if (input[0] === ')' && cache[cache.length-1] === '(') cache.pop()
-//   if (input[0] === ']' && cache[cache.length-1] === '[') cache.pop()
-//   if (input[0] === '}' && cache[cache.length-1] === '{') cache.pop()
+  // recursive case
+  if (input[0] === '(' || input[0] === '[' || input[0] === '{') cache.push(input[0])
+  if (input[0] === ')' && cache[cache.length-1] === '(') cache.pop()
+  if (input[0] === ']' && cache[cache.length-1] === '[') cache.pop()
+  if (input[0] === '}' && cache[cache.length-1] === '{') cache.pop()
 
-//   return balancedParens(input.slice(1), cache);
-// };
+  return balancedParens(input.slice(1), cache);                        // <--- eliminates non bracket chars from consideration
+};
 
 // ----- Step 3
 // refactor step 2 to eliminate non bracket characters
 // step 2 code already eliminates non bracket chars from consideration
-const balancedParens = (input, cache = []) => {
-    // base cases
-    if (input.length === 0 && cache.length === 0) return true 
-    if (input.length === 0 && cache.length > 0) return false
-    if (input[0] === ')' && cache.length === 0) return false              // <--- closing brackets with an empty cache
-    if (input[0] === ']' && cache.length === 0) return false 
-    if (input[0] === '}' && cache.length === 0) return false 
+// const balancedParens = (input, cache = []) => {
+//     // base cases
+//     return (input.length === 0 && cache.length === 0) ? true 
+//         :  (input.length === 0 && cache.length > 0) ? false
+//         :  (input[0] === ')' && cache.length === 0) ? false              
+//         :  (input[0] === ']' && cache.length === 0) ? false 
+//         :  (input[0] === '}' && cache.length === 0) ? false 
+//         :  {(input[0] === '(' || input[0] === '[' || input[0] === '{') ? cache.push(input[0]) 
+//             : (input[0] === ')' && cache[cache.length-1] === '(') ? cache.pop() 
+//             : (input[0] === ']' && cache[cache.length-1] === '[') ? cache.pop() 
+//             : (input[0] === '}' && cache[cache.length-1] === '{') ? cache.pop()
+//             return balancedParens(input.slice(1), cache) }
   
-  
-    // recursive case
-    if (input[0] === '(' || input[0] === '[' || input[0] === '{') cache.push(input[0])
-    if (input[0] === ')' && cache[cache.length-1] === '(') cache.pop()
-    if (input[0] === ']' && cache[cache.length-1] === '[') cache.pop()
-    if (input[0] === '}' && cache[cache.length-1] === '{') cache.pop()
-  
-    return balancedParens(input.slice(1), cache);                         // <--- eliminates non bracket chars from consideration
-  };
+//   };
 // test cases
 console.log(balancedParens('('));  // false
-console.log(balancedParens('()')); // true
+console.log(balancedParens('((()))')); // true
 console.log(balancedParens(')('));  // false
+console.log(balancedParens('()))'));  // false
 console.log(balancedParens('(())'));  // true
 console.log(balancedParens('[](){}')); // true
 console.log(balancedParens('[({})]'));   // true
 console.log(balancedParens('[(]{)}')); // false
 console.log(balancedParens(' const wow = { yo: thisIsAwesome() }')); // true
 console.log(balancedParens(' const newton = () => { telescopes.areSicc(); ')); // false
-
+console.log(balancedParens(' const coolFunc = () => { anotherCoolFunc(); }')); // true
+console.log(balancedParens(' const yungHash = { a_key: { another_key: a_sicc_value } ;')); // false
 
 module.exports = { balancedParens} ;
