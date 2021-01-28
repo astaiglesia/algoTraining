381992854447e58fd2afbcf72570cdf9cf6a8cfe
Alfred Sta Iglesia <a.sta.iglesia@gmail.com>
2021-01-28 09:57:13 -0800

    hack hour submission 1.28.21

--- challenges/merge-ranges.js
+++ challenges/merge-ranges.js
@@ -3,7 +3,6 @@ Write a function mergeRanges that takes an array of meeting time ranges and
 returns an array of condensed ranges, merging the overlapping intervals.
 
 Example:
-
 intervals = [[0, 1], [3, 5], [4, 8], [10, 12], [9, 10]]
 mergeRanges(intervals); -> [[0, 1], [3, 8], [9, 12]]
 
@@ -23,9 +22,40 @@ https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects
 After sorting, think about how to merge the intervals together.
 
 */
+// func accepts an array; returns an array
+// --- inputs: meeting times; 
+// --- output: condensed ranges, merge overlaps
+// # assume unordered intervals   ## assume at least one array
+// sort the intervals 
+// merge the intervals
 
-const mergeRanges = intervals => {
-  
+const mergeRanges = (intervals, output = [], sortedArr = intervals.sort((a,b) => a[0]-b[0])) => {
+//   console.log(sortedArr);
+    // base case
+    if (intervals.length === 1) return output;  // ### refactor base case! almost there -> handle end of array
+
+    // recursive case
+    // if no overlap of first element
+    let newSort;
+    if (sortedArr[0][1] < sortedArr[1][0]) {
+      output.push(sortedArr[0]);
+      newSort = sortedArr.slice(1);
+    } else {
+      // merge overlaps
+      const temp = [sortedArr[0][0], sortedArr[1][1]]
+      newSort = sortedArr.slice(2);
+      newSort.unshift(temp);
+    }
+
+    return mergeRanges(intervals, output, newSort);
 };
+ 
+
+// test cases
+intervals = [[0, 1], [3, 5], [4, 8], [10, 12], [9, 10]]
+console.log(mergeRanges(intervals)); //-> [[0, 1], [3, 8], [9, 12]]
+
+intervals = [[8, 10], [15, 18], [1, 3], [2, 6]]
+console.log(mergeRanges(intervals)); //-> [[1, 6], [8, 10], [15, 18]]
 
 module.exports = {mergeRanges};
