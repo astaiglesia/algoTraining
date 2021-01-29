16e7826bd6d2cb60a2f293da82da5ce464c10adf
Alfred Sta Iglesia <a.sta.iglesia@gmail.com>
2021-01-29 09:58:35 -0800

    hack hour submission 1.29.21

--- challenges/apple-stocks.js
+++ challenges/apple-stocks.js
@@ -20,8 +20,41 @@ calculate profit along the array.
 
 */
 
-const highestProfit = apple_stock => {
+// function accepts an array; returns a value
+// --- input array contains values in chrono order
+// --- iterate through array to find the greatest difference between 2 elements
+// ------ store differences in a cache; return Math.max of collection?
+// edge cases/considerations: 
+// - no need to track inputs
+// - no possibility of profit -> return 0 if no profit is possible (i.e. decreasing values throughout array) 
+// - or invalid input -> i.e. values are not a number
+// ### USE max O(n^2) time complexity 
 
+const highestProfit = (apple_stock, storage = []) => {
+  if (typeof apple_stock[0] !== 'number' ) return 0                                         // invalid inputs ### refactor for edge cases
+  if (apple_stock.length === 1) return Math.max(...storage)                                 // base case    
+  
+  if (apple_stock[0] > apple_stock[1] ) return highestProfit(apple_stock.slice(1), storage)  // decreasing stock value
+
+  // increasing stock value -> check if the subsequent values are increasing
+  for (let i = 1; i < apple_stock.length || apple_stock[i] < apple_stock[0]; i++) {
+    if (apple_stock[0] < apple_stock[i]) {
+        storage.push(apple_stock[i] - apple_stock[0]);
+    }
+  }
+  
+  return highestProfit(apple_stock.slice(1), storage)
 }
+
+// test cases
+console.log(highestProfit({0: 10, 1: 5, 2: 0})) //toEqual(0);
+console.log(highestProfit('stocks')) //toEqual(0);
+console.log(highestProfit(1000)) //toEqual(0);
+
+stocks = [0, 2000, 4000, 6000, 8000, 10000];
+console.log(highestProfit(stocks)); //toEqual(10000);
+
+stocks = [1000, 500, 1000, 1500, 0, 200, 800, -10, 0, 100];
+console.log(highestProfit(stocks)) // toEqual(1000);
 
 module.exports = {highestProfit}
