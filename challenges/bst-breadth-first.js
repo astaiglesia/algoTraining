1041e69e43f9632df44ef0470d14d56844ee9661
Alfred Sta Iglesia <a.sta.iglesia@gmail.com>
2021-02-19 09:58:51 -0800

    hh submission 2.19.21

--- challenges/bst-breadth-first.js
+++ challenges/bst-breadth-first.js
@@ -1,43 +1,72 @@
-function BinarySearchTree(value) {
-  this.value = value;
-  this.right = null;
-  this.left = null;
-}
 
 /*
-
 Given the root of a binary search tree and a callback function, apply the
 callback to the values of the BST in breadth-first order.
 
-Example:
-
-If the tree is
-
-     4
-   /   \
-  2     7
- / \     \
-1   3     9
-         /
-        8
-
-then apply the callback on the numbers in the order:
-4, 2, 7, 1, 3, 9, 8.
-
 Hint:
-
 Maintain a queue (array) consisting of the nodes we need to operate on.
 For each node in the queue, push the left and right children (if applicable) to
 the end of the queue. Keep consuming the queue (using the shift method) until
 there are no more nodes in the queue.
 
 Utilizing recursion is not necessary, nor recommended.
+Example:
+
+If the tree is
+
+4
+/   \
+2     7
+/ \     \
+1   3     9
+/
+8
+
+then apply the callback on the numbers in the order:
+4, 2, 7, 1, 3, 9, 8.
 
 */
 
+function BinarySearchTree(value) {
+  this.value = value;
+  this.right = null;
+  this.left = null;
+}
+// given: bst root, callback; return: none stated
+// -- func to invoke call back to the values of the bst 
+// -- traverse breadth first, storing values to the queue 
+// -- create a queue (array)
+// ---- (if exists) push the left child to queue, push the right child
+// -- NO RECURSION
+// invoke callback forEach on the array 
+// ## edge cases
 const bfs = (root, callback) => {
+  const bstValues = [];
   
+  while (this)
+  bstValues.push(root.value);
+  bstValues.push(root.left.value);
+  bstValues.push(root.right.value);
+
+
+  console.log(bstValues);
+
+
 };
+
+
+// create a test case
+bst = new BinarySearchTree(4);
+bst.left = new BinarySearchTree(2);
+bst.right = new BinarySearchTree(7);
+bst.left.left = new BinarySearchTree(1);
+bst.left.right = new BinarySearchTree(3);
+bst.right.right = new BinarySearchTree(9);
+bst.right.right.left = new BinarySearchTree(8);
+
+console.log(bfs(bst));
+
+
 
 /*
 
