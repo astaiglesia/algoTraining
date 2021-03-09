91887e894be0880f560f7033d5f0f5f57d03c1f4
Alfred Sta Iglesia <a.sta.iglesia@gmail.com>
2021-03-09 09:55:28 -0800

    hh submission 3.9.21

--- challenges/longest-common-prefix.js
+++ challenges/longest-common-prefix.js
@@ -8,8 +8,36 @@ longestCommonPrefix(["dog","racecar","car"]) --> "" (There is no common prefix a
 
 */
 
-const longestCommonPrefix = (strs) => {
+// input: array of strings; return: longest common prefix among strings
+// iterate through elements
+// edge cases: no prefix
+
+const longestCommonPrefix = (strs, index = 0, output = "") => {
+
+  console.log(`current index is ${index} current char is ${strs[0][index]} current output is ${output}`);
+  // ----- recursive case 
+  // check char of ele 0 at index against matching index of all other elements
+  // if char exists on .every element, concat with output
+  const currentCheck = strs.every( current => {strs[0][index] === current[index]});
+  
+  console.log(currentCheck);
+  
+  if (currentCheck === true) {
+    output.concat(strs[0][index]); 
+    return longestCommonPrefix(strs, i + 1, output)
+  } else {
+    return output;
+  }
+
+
   
 };
+
+// test case
+console.log(longestCommonPrefix(["flower","flow","flight"])); // --> "fl"
+
+
+
+
 
 module.exports = { longestCommonPrefix };
