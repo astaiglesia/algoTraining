b1984190424d841a10d2f6c7d87e1151a600d590
Alfred Sta Iglesia <a.sta.iglesia@gmail.com>
2021-03-17 09:56:57 -0700

    hh submission 3.17.21

--- challenges/generate-parentheses.js
+++ challenges/generate-parentheses.js
@@ -28,8 +28,33 @@ Given n = 0, a solution set is:
 
 */
 
+// input:	an integer; output: an array of strings
+// find: all combinations of well formed parens in the format of strings
+// givens: an intetegers
+// assumptions: none
+// edge case: invalid input (non-integer)
+// approach: recursive / iterative
+// - assign an empty array as an output
+// - initialize an empty string which will compose of n pairs of parens
+// -- iterate through the possible combinations by concating parens to the string
+// -- push each combination to output when parenString length/2 equals n
+// -- return output when all combinations have been formed ---> ### do we need a stack to hold closing parens?
+// --- if n = 0, return the empty string 
+// --- if n = 1, cancat 1 open
+
+
+// ------- Code --------
+
 const generateParentheses = n => {
   
 };
+
+// test cases:
+console.log( ); // <--- expect
+console.log( ); // <--- expect
+
+
+// ------- Refactor --------
+
 
 module.exports = {generateParentheses};
