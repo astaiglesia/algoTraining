771b80d4f6ccfd3132f0e11ffcab29b5384aa92f
Alfred Sta Iglesia <a.sta.iglesia@gmail.com>
2021-03-18 09:47:17 -0700

    hh submission 3.18

--- challenges/get-all-products.js
+++ challenges/get-all-products.js
@@ -20,8 +20,43 @@ input array? How would you handle this?
 
 */
 
+// input: array (arbitrary number of at least 2 integers); output: array (of integers)
+// find: all products possible by multiplying all but one of the array elements with each other
+// givens: integers may be +, -, or 0
+// breakdown: MULTIPLY ALL BUT ONE
+// assumptions:
+// edge cases: invalid inputs, 
+
+// approach: iterate/recurse through array -> multiply all numbers in the array EXCEPT for the current element
+// create an output array
+// -- on each pass
+// ---- reduce the array to a single number and divide that value by the current element
+// ---- push the eval result to output
+
+// ------- Code --------
+
 const getAllProducts = array => {
+    const output = [];
+    const arrayProduct = array.reduce( (accum, current) => accum * current);
+    
+    for (let i = 0; i < array.length; i++) {
+        output.push(arrayProduct/array[i]);
+  }
   
+  return output;
 };
+
+
+// test cases:
+console.log(getAllProducts([1, 7, 3, 4])); // <--- expect -> [84, 12, 28, 21] via: [7*3*4, 1*3*4, 1*7*4, 1*7*3]
+console.log(getAllProducts([2, 5, 3])); // <--- expect -> [15, 6, 10] via: [5*3, 2*3, 2*5]
+console.log(getAllProducts([0, 3, 4, 0])); // [0, 0, 0, 0]
+console.log(getAllProducts([0, 3, 4, 2])); // [0, 0, 0, 24]
+
+// ------- Refactor --------
+// refactored -> used reduce only once set to a constant
+// ## refactor -> currently the code is not verified to handle zero's and negatives
+// ---- use recursion 
+
 
 module.exports = {getAllProducts};
