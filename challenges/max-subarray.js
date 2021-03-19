d0dcc66f966c3bca96d9d392dd9d5b5052ccf4e0
Alfred Sta Iglesia <a.sta.iglesia@gmail.com>
2021-03-19 09:55:37 -0700

    hh submission 3.19.21

--- challenges/max-subarray.js
+++ challenges/max-subarray.js
@@ -11,18 +11,45 @@ A valid subarray is any slice of consecutive elements from the array.
 Find the subarray with the largest sum from the input array.
 
 e.g.
-input = [1, -2, 3, 10, -4, 7, 2, -5]
-maxSubarray(input); 
-// returns 18 from subarray [3, 10, -4, 7, 2]
 
-input2 = [15, 20, -5, 10]
-maxSubarray(input2); 
-// returns 40 from subarray [15, 20, -5, 10]
 */
 
-const maxSubarray = (arr) => {
+// |===> Solution <===|
+// input:	array (of integers); output: array (of integers)
+// given: input array contains both pos & neg integers
+//        subarrays are any slices from an array comprised of consecutive elements from the input
+// find: subarray with the largest sum
+// assumptions: 
+// breakdown: create and reduce(sum) all valid subarrays -> RETURN THE SUBARRAY that evaluates to the greatest sum
+//            -or- slice the smallest value and return that array
+// edge cases: invalid inputs, ?input array contains 0?
+
+// approach: 
+// set up an output array
+// IF all elements in the input are non-zero return the input
+// ELSE ??? determine the smallest element??
+// Math.min the array, get the index of -> this negative integer should not be part of the subarray
+// splice results to left and right of min val 
+// and recurse left and right ... finish determining logic
+// return the resulting array
+
+// ------- Code --------
+
+const maxSubarray = (arr, output = []) => {
+  
 
 }
+
+// test cases:
+input = [1, -2, 3, 10, -4, 7, 2, -5]; 
+input2 = [15, 20, -5, 10]; 
+
+console.log(maxSubarray(input)); // <--- expect returns 18 from subarray [3, 10, -4, 7, 2]
+console.log(maxSubarray(input2)); // <--- expect returns 40 from subarray [15, 20, -5, 10]
+
+
+// ------- Refactor --------
+
 
 /*
   Extension: solve in linear time and constant space
