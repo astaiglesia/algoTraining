d54c4825718cebd9ad9f76097bbaa9f9f2c19f94
Alfred Sta. Iglesia <a.sta.iglesia@gmail.com>
2021-11-20 17:32:38 -0500

    complete drawStairs - brute force and tco recursion

--- challenges/take2/2-draw-stairs.js
+++ challenges/take2/2-draw-stairs.js
@@ -16,8 +16,86 @@ drawStairs(6) ->
 ******
 
 */
+//---------------------------------take2
+// input: num
+// output: no eval return (console log strings)
+// givens: non-negative input; each level is num length; stairs is num height
+// approach: nested for loop, recursion with looping
+// - build the string at each level by concat asterisks and spaces
+// - build stairs from top -> down; use nested iteration
+// - use loop counter to represent asterisks to concat
+// - use num minus counter for spapces
+// - increment on each pass
+// - terminate loop / recursion when loop counter equals num
+// edge cases: 
+// - invalid input (assume valid?)
+// - if num = 0?
+
+// brute force
+// const drawStairs = num => {
+//   if (typeof num !== "number") return "invalid input";
+  
+//   let currentFlight = "";
+//   if (num < 0) console.log(currentFlight); 
+  
+//   for (let i = 1; i <= num; i++) {
+//     // concat the spaces
+//     for (let spaces = 0; spaces < (num - i); spaces++) {
+//       currentFlight += " ";
+//     }
+
+//     // concat the asterisks
+//     for (let asterisks = 0; asterisks < i; asterisks++) {
+//       currentFlight += "*";
+//     }
+
+//     // log the flight and reset
+//     console.log(currentFlight);
+//     currentFlight = "";
+//   }
+  
+//   return "end of challenge";
+// }
 
 
+// time: O(n2) -> Quadratic time -> sequential looping nested within a loop
+// space: O(1) -> constant space allocation regardless of input
+
+// alt approach -> use tco recursion to eliminate the nested loop => time O(n)
+const spaceHelper = (num, asterisks, spaces, currentFlight) => {
+  return (spaces === 0) ? currentFlight : (currentFlight += " ", (spaceHelper(num, asterisks, spaces - 1, currentFlight)));
+}
+ 
+const asteriskHelper = (num, asterisks, spaces, currentFlight) => {
+  return (asterisks === 0) ? currentFlight : (currentFlight += "*", (asteriskHelper(num, asterisks - 1, spaces, currentFlight)));
+}
+
+const drawStairs = (num, asterisks = 1, spaces = num - asterisks, currentFlight = "") => {
+  if (typeof num !== "number") return "invalid input";
+  if (num < 0) console.log(currentFlight); 
+  if (asterisks > num) return "end of challenge";
+  
+  // recursive case:
+  // concat the spaces
+  currentFlight = spaceHelper(num, asterisks, spaces, currentFlight);
+
+  // concat the asterisks
+  currentFlight = asteriskHelper(num, asterisks, spaces, currentFlight);
+
+  // log the flight and recurse
+  console.log(currentFlight);
+  
+  return drawStairs(num, asterisks + 1);
+}
+
+// test cases
+// console.log(drawStairs(5));
+// console.log(drawStairs(-4));
+// console.log(drawStairs("tom"));
+
+
+
+//---------------------------------previous takes
 /* |===> Solution <===|
   input: target: number
   output: level: string
@@ -31,26 +109,26 @@ drawStairs(6) ->
   refactored time: 
 */
 
-const drawStairs = target => {
+// const drawStairs = target => {
   
-  // loop for each level - re-initialize level
-  for (let i = 1; i <= target; i++) {
-    let level = ' ';
+//   // loop for each level - re-initialize level
+//   for (let i = 1; i <= target; i++) {
+//     let level = ' ';
     
-    // loop for each space
-    for (let j = target-i; j > 0; j--) {
-      level = level.concat(' ')
-    }
+//     // loop for each space
+//     for (let j = target-i; j > 0; j--) {
+//       level = level.concat(' ')
+//     }
      
-    // loop for each star
-    for (let k = i; k > 0; k--) {
-      level = level.concat('*')
-    }
+//     // loop for each star
+//     for (let k = i; k > 0; k--) {
+//       level = level.concat('*')
+//     }
 
-    console.log(level);
-  };
+//     console.log(level);
+//   };
 
-};
+// };
 
 
 // const drawStairsRecursive = (target, stars = 1, level = ' ', spaces = target-stars) => {
@@ -65,9 +143,9 @@ const drawStairs = target => {
 
 
 // test cases:
-drawStairs('negative 5', -5); // expected => shown in example
-drawStairs('zero', 0); // expected => shown in example
-drawStairs(6); // expected => shown in example
+// drawStairs('negative 5', -5); // expected => shown in example
+// drawStairs('zero', 0); // expected => shown in example
+// drawStairs(6); // expected => shown in example
 
 
 
