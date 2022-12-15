453c43a374c674651a84a23a68bf457f6242be77
Alfred Sta. Iglesia <a.sta.iglesia@gmail.com>
2022-12-15 17:43:03 -0500

    refactor minPathMatrix - handle dead ends, open floor plan

--- assessments/minPathMatrix.js
+++ assessments/minPathMatrix.js
@@ -26,91 +26,91 @@ area = [[1, 0, 0],
 	[1, 0, 0],
 	[1, 9, 1]]
 
-
-input:numRows: num, numColumns: num, area: 2d array
-output: num
-givens: 
-- area is a uniform 2d array representing a grid when elements are stacked vertically
-- numRows is the length of the subarrays
-- numColumns is the length of area
-- area[0][0] = origin, 1 = road, 0 = no road, 9 = destination
-- 4pt moves allowed to find a road to the destination
--> define motions, params range from 0 thru length
-  - left = col -= 1
-  - right = col += 1
-  - up = row -= 1
-  - down = row += 1
-approach: 
-- track current and prev steps? (array?)?.length
-- starting with origin - analyze 4 directions
-  - recurse to cells with road !== prev step
-- terminate recursion if:
-  - current element is 9 || if 3 directions away from origin miss roads
-- 
--
-edges: invalid inputs -we need a proper typed and sized grid 
-- no destination
-- no path to destination
-
-timespace: 
-linear time - visit all elements in worst case
-linear space - frames for each step
-
-numRows = 3	3
-numColumns = 3
-area = [[1, 0, 0],
-	      [1, 0, 0],
-	      [1, 9, 1]]
-
--> define motions, params range from 0 thru length
-  - left = col -= 1
-  - right = col += 1
-  - up = row -= 1
-  - down = row += 1
-  console.log('------- new step at row', row, 'col', col, 'current step:', area[row][col], 'with count of:', stepCounter )
 */ 
 
-const minPathMatrix = (numRows, numColumns, area, stepCounter = 0, row = 0, col = 0, prevRow, prevCol, stepTaken = false) => {
-  // basecase
-  if (area[row][col] === 9) return stepCounter;
+const findShortestPath = (numRows, numColumns, area) => {
+  class PathForward {
+    constructor(row, col, stepCounter, newGrid) {
+      this.newRow = row,
+      this.newCol = col,
+      this.stepCounter = stepCounter,
+      this.newGrid = newGrid
+    }
+  }
   
-  // recursive cases
-  // move left - if not out of bounds, not the prevStep, is a road
-  let newCol = col - 1,
-      newStep = area[row][newCol];
-  if (newCol !== prevCol && newCol > 0 && newStep === 1 || newStep === 9) {
-    stepTaken = true;
-    return minPathMatrix(numRows, numColumns, area, stepCounter + 1, row, newCol, row, col);
+  const queue = [];
+  const paths = [];
+  
+  // 
+  const minPathMatrix = (numRows, numColumns, area, stepCounter = 0, row = 0, col = 0) => {
+    let newCol, newRow, newGrid = [...area];
+    // update completed paths & queue for additional routes
+    if (area[row][col] === 9) paths.push(stepCounter)
+    else {   
+      newGrid[row][col] = 0;  // marks location as visited on current path
+      
+      newCol = col - 1;       // check left
+      newCol > 0 && area[row][newCol] && queue.unshift(new PathForward(row, newCol, stepCounter, newGrid))
+      newCol = col + 1;       // check right
+      newCol < numColumns && area[row][newCol] && queue.unshift(new PathForward(row, newCol, stepCounter, newGrid));
+      
+      newRow = row - 1;       // check up
+      newRow > 0 && area[newRow]?.[col] && queue.unshift(new PathForward(newRow, col, stepCounter, newGrid));
+      newRow = row + 1;       // check down
+      newRow < numRows && area[newRow]?.[col] && queue.unshift(new PathForward(newRow, col, stepCounter, newGrid));
+    } 
+
+    // basecase - when queue is empty
+    return (!queue.length) ? (paths.length ? Math.min(...paths) : -1)
+    : (
+      nextStep = queue.pop(), 
+      minPathMatrix(numRows, numColumns, nextStep.newGrid, nextStep.stepCounter + 1, nextStep.newRow, nextStep.newCol)
+    );
   }
 
-  // move right
-  newCol = col + 1,
-  newStep = area[row][newCol];
-  if (newCol !== prevCol && newCol < numColumns && newStep === 1 || newStep === 9) {
-    stepTaken = true;
-    return minPathMatrix(numRows, numColumns, area, stepCounter + 1, row, newCol, row, col);
-  }
-
-  // move up
-  let newRow = row - 1;
-      newStep = area[newRow]?.[col];
-      if (newRow !== prevRow && newRow > 0 && area[newRow][col] === 1 || newStep === 9) {
-        stepTaken = true;
-    return minPathMatrix(numRows, numColumns, area, stepCounter + 1, newRow, col, row, col);
-  }
-  
-  // move down
-  newRow = row + 1;
-  newStep = area[newRow]?.[col];
-  if (newRow !== prevRow && newRow < numRows && area[newRow][col] === 1 || newStep === 9) {
-    stepTaken = true;
-    return minPathMatrix(numRows, numColumns, area, stepCounter + 1, newRow, col, row, col);
-  }
-  
-  // handle no path base case
-  if (stepTaken === false) return -1;
+  return minPathMatrix(numRows, numColumns, area);
 }
 
+
+/*
+  input:numRows: num, numColumns: num, area: 2d array
+  output: num
+  givens: 
+  - area is a uniform 2d array representing a grid when elements are stacked vertically
+  - numRows is the length of the subarrays
+  - numColumns is the length of area
+  - area[0][0] = origin, 1 = road, 0 = no road, 9 = destination
+  - 4pt moves allowed to find a road to the destination
+  -> define motions, params range from 0 thru length
+    - left = col -= 1
+    - right = col += 1
+    - up = row -= 1
+    - down = row += 1
+  approach: 
+  - implement a NEEDS A QUEUE for Matrix BFS
+  - on each pass instantiate new nodes to describing next step to additional routes
+    - starting with origin - analyze 4 directions
+    - enqueue a new node for recursion, props:
+      - new row + col to analyze
+      - updated stepcounter 
+      - updated grid passed into recursive call to reflect visited cells()
+      - 
+    - handle path hits
+  - terminate recursion on empty queue
+  - 
+  -
+  edges: invalid inputs -we need a proper typed and sized grid 
+  - no destination
+  - no path to destination
+  - handle multiple paths
+  - handle dead ends included
+
+  timespace: 
+  linear time - visit all elements in worst case
+  linear space - frames for each step
+
+  // console.log('------- new step at row', row, 'col', col, 'current step:', area[row][col], 'with count of:', stepCounter ) 
+*/ 
 
 
 // test cases
@@ -118,18 +118,18 @@ let numRows = 3,
     numColumns = 3,
     area = [[1, 0, 0],
 	          [1, 0, 0],
-	          [1, 9, 1]]
+	          [1, 9, 1]];
 
-console.log('handles path to 9 - expect', minPathMatrix(numRows, numColumns, area), 'to be 3')
+console.log('handles path to 9 - expect', findShortestPath(numRows, numColumns, area), 'to be 3');
 
 numRows = 4, 
 numColumns = 3,
 area = [[1, 0, 0],
         [1, 0, 0],
         [0, 0, 0],
-        [1, 9, 1]]
+        [1, 9, 1]];
 
-console.log('handles no paths - expect', minPathMatrix(numRows, numColumns, area), 'to be -1')
+console.log('handles no paths - expect', findShortestPath(numRows, numColumns, area), 'to be -1');
 
 numRows = 5, 
 numColumns = 6,
@@ -137,9 +137,35 @@ area = [[1, 0, 0, 0, 0, 0],
         [1, 0, 1, 1, 1, 1],
         [1, 0, 1, 0, 0, 1],
         [1, 0, 1, 0, 0, 1],
-        [1, 1, 1, 0, 9, 1]]
+        [1, 1, 1, 0, 9, 1]];
+console.log('handles winding paths - expect', findShortestPath(numRows, numColumns, area), 'to be 16');
 
-console.log('handles winding paths - expect', minPathMatrix(numRows, numColumns, area), 'to be 16')
+numRows = 5, 
+numColumns = 6,
+area = [[1, 0, 0, 1, 0, 0],
+        [1, 1, 1, 1, 0, 0],
+        [1, 0, 0, 1, 0, 9],
+        [1, 0, 0, 0, 0, 1],
+        [1, 1, 1, 1, 1, 1]];
+console.log('handles dead ends - expect', findShortestPath(numRows, numColumns, area), 'to be 11');
+
+numRows = 5, 
+numColumns = 6,
+area = [[1, 1, 1, 0, 0, 0],
+        [1, 0, 9, 1, 1, 0],
+        [1, 0, 1, 0, 1, 0],
+        [1, 0, 1, 0, 1, 0],
+        [1, 1, 1, 1, 1, 0]];
+console.log('handles mutliple paths - expect', findShortestPath(numRows, numColumns, area), 'to be 3');
+
+numRows = 5, 
+numColumns = 6,
+area = [[1, 1, 1, 0, 0, 0],
+        [1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 0],
+        [1, 0, 1, 0, 1, 9],
+        [1, 1, 1, 1, 1, 0]];
+console.log('handles open floor plan - expect', findShortestPath(numRows, numColumns, area), 'to be 8');
 
 
-module.exports = {minPathMatrix};
+module.exports = {findShortestPath};
