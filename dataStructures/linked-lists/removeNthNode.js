a28ffddd988d4aedecdff28cc809c30c04397b9d
Alfred Sta. Iglesia <a.sta.iglesia@gmail.com>
2022-12-19 13:49:51 -0500

    remove nth node

--- dataStructures/linked-lists/removeNthNode.js
+++ dataStructures/linked-lists/removeNthNode.js
@@ -27,7 +27,7 @@
 */ 
 
 
-function helper(head) {
+const getLength = (head) => {
   let counter = 0,
       current = head;
 
@@ -38,49 +38,52 @@ function helper(head) {
 
   return counter;
 }
+
+const removeNthFromEnd = (head, n, prevNode = null, counter = getLength(head) + 1 - n) => {
+  return (counter < 1 || !n || !head) ? head
+    : (counter === 1 && n === 1 && !prevNode) ? (
+      head = prevNode,
+      head
+    ) : (counter === 1) ? (
+      temp = head,
+      prevNode.next = head.next,
+      temp.next = null,
+      head
+    ) : (
+      newPrev = head,
+      removeNthFromEnd(head.next, n, newPrev, counter - 1)
+    )
+}
+
 //                        1     1                       
-const removeNthFromEnd = (head, n, current = head, prevNode = null, length = helper(head), counter = length + 1 - n) => {
-    console.log(length)
-    console.log('----------- new call on the stack (head:', head, 'n: ', n, 'prevNode: ', prevNode, 'counter: ', counter)
-    if (counter < 1 || !head) return head;                     // handles edge cases (tests 3)
-                                  // targeting undefined returns input (test4)
+// const removeNthFromEnd = (head, n, current = head, prevNode = null, length = getLength(head), counter = length + 1 - n) => {
+//     console.log(length)
+//     console.log('----------- new call on the stack (head:', head, 'n: ', n, 'prevNode: ', prevNode, 'counter: ', counter)
+//     if (counter < 1 || !head || !n) return head;                     // handles edge cases (tests 3)
+//                                   // targeting undefined returns input (test4)
 
-    // handle targeting the only node in a LL
-    // let temp = head;
-    if (counter === 1 && n == 1 && !prevNode) {
-      return null
-    }          
+//     // handle targeting the only node in a LL
+//     // let temp = head;
+//     if (counter === 1 && n == 1 && !prevNode) {
+//       head = prevNode
+//       current = null;
+//       console.log(' ----- trigger single node handler ----- updated head: ', head, current)
+//       return current;
+//     }          
     
-    // base case - terminate when counter hits 1
-    if (counter === 1) {
-        const temp = current;
-        prevNode.next = current.next
-        temp.next = null
-        return head
-    }
+//     // base case - terminate when counter hits 1
+//     if (counter === 1) {
+//         const temp = current;
+//         prevNode.next = current.next
+//         temp.next = null
+//         return head
+//     }
 
-    // recursive case
-    const newPrev = current;
-    // console.log(newPrev)
-    return removeNthFromEnd(head, n, current.next, newPrev, length,  counter - 1)
-};
-
-// const removeNthFromEnd = (head, n, prevNode = null, counter = helper(head) + 1 - n) => {
-//   return (!n || !head) ? (console.log('edge cases--->'), head)
-//     : (counter === 1 && !prevNode) ? (        console.log('hit++++'),
-//       temp = head,
-//       head = temp.next
-//     ) : (counter === 1) ? (
-//       temp = head,
-//       prevNode.next = head.next,
-//       temp.next = null,
-//       head
-//     ) : (
-//       newPrev = head,
-//       removeNthFromEnd(head.next, n, newPrev, counter - 1)
-//     )
-// }
-
+//     // recursive case
+//     const newPrev = current;
+//     // console.log(newPrev)
+//     return removeNthFromEnd(head, n, current.next, newPrev, length,  counter - 1)
+// };
 
 
 
@@ -92,38 +95,28 @@ function ListNode(val, next) {
   this.next = (next===undefined ? null : next)
 }
 
+const tester1 = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))))
+removeNthFromEnd(tester1, 2)
+console.log('+++TEST1 expect', tester1, 'to be 1 > 2 > 3 > 5')
 
+console.log('+++TEST2 3rd value now points to the 5th value: expect ', tester1.next.next, 'to be 3 > 5')
 
+removeNthFromEnd(tester1, 2)
+console.log('+++TEST3 expect', tester1, 'to be 1 > 2 > 5')
+console.log('+++TEST4 expect targeting 0 ', tester1, 'to be same as input: 1 > 2 > 5')
 
-// const tester1 = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))))
-// console.log('----- tester1', tester1)
-
-// removeNthFromEnd(tester1, 2)
-// console.log('TEST1 expect', tester1, 'to be 1 > 2 > 3 > 5')
-
-// console.log('expect 4th value has been removed: ', tester1.next.next, 'to be 3 > 5')
-
-// removeNthFromEnd(tester1, 2)
-// console.log('TEST2 expect', tester1, 'to be 1 > 2 > 5')
-// console.log('TEST3 expect targeting 0 ', removeNthFromEnd(tester1, 0), 'to be same as input: 1 > 2 > 5')
-
-// console.log('TEST4 expect target > length', removeNthFromEnd(tester1, 4), 'to be same as input: 1 > 2 > 5')
-
-
-
-console.log('-------------')
+console.log('+++TEST5 expect target > length', tester1, 'to be same as input: 1 > 2 > 5')
 
 let tester2 = new ListNode(1)
-removeNthFromEnd(tester2, 1)
-console.log('TEST5 expect handle removing single node: ', tester2, 'to be null')
+console.log('+++TEST6 expect handle removing single node: ', removeNthFromEnd(tester2, 1), 'to be null')
 
 let tester3 = new ListNode(1, new ListNode(2))
-tester2 = removeNthFromEnd(tester3, 1)
-console.log('TEST6 expect', tester3, 'to be 1')
+removeNthFromEnd(tester3, 1)
+console.log('+++TEST7 expect', tester3, 'to be 1')
 
 
 let tester4 = null
 removeNthFromEnd(tester4, 1)
-console.log('TEST7 expect null input: ', tester4, 'to be null')
+console.log('+++TEST8 expect null input: ', tester4, 'to be null')
 
 
